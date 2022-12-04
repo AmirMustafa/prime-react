19ab4eebd64c22dd6e0e62ee2dea9a47e213d4ac
Amir Mustafa <amirengg15@gmail.com>
2022-12-04 18:30:10 +0530

    feat: adding prime react button, google font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Button } from "primereact/button";
 
 const App = () => {
   return (
     <div className="App">
       <h2>App Component</h2>
+      <Button label="Click" icon="pi pi-check" />
+
+      <Button label="Primary" />
+      <Button label="Secondary" className="p-button-secondary" />
+      <Button label="Success" className="p-button-success" />
+      <Button label="Info" className="p-button-info" />
+      <Button label="Warning" className="p-button-warning" />
+      <Button label="Danger" className="p-button-danger" />
     </div>
   );
 };
