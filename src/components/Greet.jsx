71e8b1523964671556f099f17183e0faf2e78a8b
Amir Mustafa <amirengg15@gmail.com>
2022-12-07 02:09:35 +0530

    feat: toast message on button click

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
 
 const Greet = () => {
+    let toast = useRef(null);
 
     const [state, setState] = useState({
         msg: ''
     });
 
-    const greet = () => {
-        alert(state.msg);
+    const greet = (event) => {
+        event.preventDefault();
+        toast.current.show({severity: 'success', summary: 'Success Message', detail: state.msg});
     };
     return (
         <>
@@ -25,6 +28,7 @@ const Greet = () => {
                         <Button onClick={greet} label={'Greet'} className="p-button-success ml-2" /> 
                     </form>
                 </Card>
+                <Toast ref={toast} />
                 </div>
             </div>
         </>
