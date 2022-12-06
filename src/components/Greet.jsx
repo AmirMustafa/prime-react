3a3f4326050d8b67bb178030750b25d48c3f1052
Amir Mustafa <amirengg15@gmail.com>
2022-12-07 01:51:09 +0530

    feat: handling data on button click - alert

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 
 const Greet = () => {
+
+    const [state, setState] = useState({
+        msg: ''
+    });
+
+    const greet = () => {
+        alert(state.msg);
+    };
     return (
         <>
             <div className='grid mt-2' >
                 <div className='col-4'>
                 <Card className='bg-black-alpha-20'>
                     <form>
-                        <InputText placeholder='Message'/>
-                        <Button label={'Greet'} className="p-button-success ml-2" /> 
+                        <InputText 
+                            value={state.msg} 
+                            onChange={(e) => setState({...state, msg: e.target.value})}
+                            placeholder='Message'/>
+                        <Button onClick={greet} label={'Greet'} className="p-button-success ml-2" /> 
                     </form>
                 </Card>
                 </div>
