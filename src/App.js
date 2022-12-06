97955152ca577d27c8d53ef29dfea35000888213
Amir Mustafa <amirengg15@gmail.com>
2022-12-07 00:27:14 +0530

    feat: counter component, flex grid system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
-import { Button } from "primereact/button";
+import { Menubar } from "primereact/menubar";
+import Counter from "./components/Counter";
 
 const App = () => {
   return (
     <div className="App">
-      <h2>App Component</h2>
-      <Button label="Click" icon="pi pi-check" />
-
-      <Button label="Primary" />
-      <Button label="Secondary" className="p-button-secondary" />
-      <Button label="Success" className="p-button-success" />
-      <Button label="Info" className="p-button-info" />
-      <Button label="Warning" className="p-button-warning" />
-      <Button label="Danger" className="p-button-danger" />
+      <Menubar
+        className="bg-blue-600 p-3 text-3xl text-white"
+        start={"Prime React"}
+      />
+      <Counter />
     </div>
   );
 };
---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <>
+            <div class="grid">
+                <div className='col-4'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda, recusandae non quos accusantium vero odio fugiat quis cumque, quo provident optio beatae debitis tempore dolore necessitatibus pariatur officiis eum?
+                </div>
+            </div>
+        </>
+    );
+}
+export default Counter;
