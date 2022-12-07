f9e26cd225d482565a7e35e5a419adeafc2df5f4
Amir Mustafa <amirengg15@gmail.com>
2022-12-07 22:25:54 +0530

    feat: adding User service using axios

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { UserService } from '../service/User';
 
 const UserList = () => {
+
+    let [state, setState] = useState({
+        loading: false,
+        users: [],
+        error: null
+    });
+
+    useEffect(async () => {
+        try {
+            setState({...state, loading: true});
+            const response = await UserService.getAllUsers();
+            setState({...state, users: response, loading: false});
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+            setState({...state, error: err});
+        }
+    }, []);
+
     return (
         <>
             <h2>User List</h2>
